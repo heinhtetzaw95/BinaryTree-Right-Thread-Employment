4d935a3575a12af1cdd73b3a5b23cc453da69fa6
Hein Htet Zaw <thanlone1995@gmail.com>
2016-04-22 02:43:49 -0500

    left-thread for now (Migrated from previous program)

---
+++ Binary Tree_Right-Threads/tree_header.h
@@ -0,0 +1,45 @@
+#ifndef TREE_HEADER_H_
+#define TREE_HEADER_H_
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+			//create variables to access input and output files
+	static ifstream input("tree_sample.txt", ios::in);
+	static ofstream output("output_sample.txt", ios::out);
+
+			//construct a structure for items
+struct item {
+	string ID, description;
+	int onHand, onOrder, quantity;
+
+			//include left and right pointers for each node
+	item *left, *right;
+};
+
+class tree {
+		//declare private variables
+private:
+	item *root;
+
+		//declare public functions
+public:
+	tree() { root = NULL; }						//make constructor function
+
+	bool add(item);								//make function to add nodes
+	bool del(item);								//make function to delete nodes
+	bool sell(item);							//make function to simulate selling items
+	bool order(item);							//make function to simulate ordering more items
+	bool receive(item);							//make function to simulate reciving orders
+
+	void patchParent(item*, item*, item*);		//make function to rearrange the nodes when delete
+
+	item * search(item);						//make function for searching if an item exists
+	item * getRoot() { return root; };			//make function for identifying the root node
+
+};
+
+#endif // !TREE_HEADER_H_
+
---
+++ Binary Tree_Right-Threads/tree_right.cpp
@@ -0,0 +1,192 @@
+//************************************  PROGRAM IDENTIFICATION  ***************************************
+//*                                                                                                   *
+//*   PROGRAM FILE NAME: tree_right.cpp              ASSIGNMENT #: 8               Grade: _________   *
+//*                                                                                                   *
+//*   PROGRAM AUTHOR:     _________________________________________                                   *
+//*                                    Hein Htet Zaw                                                  *
+//*                                                                                                   *
+//*   COURSE #:  CSC 36000 11                                                  DUE DATE: May 6, 2016  *
+//*                                                                                                   *
+//*****************************************************************************************************
+//*****************************************************************************************************
+//*                                                                                                   *
+//*  Program description: This program works exactly the same as program 7, but this program employs  *
+//*                       right-threads to build and traverse the Binary Tree using InOrder Traversal *
+//*                       Data file name will be thread_in.txt and the output should be the same      *
+//*                       format as Program 7.                                                        *
+//*                                                                                                   *
+//*****************************************************************************************************
+#include <iostream>
+#include <string>
+
+#include "tree_header.h"
+#include "tree_helpers.cpp"
+
+using namespace std;
+
+int main() {
+			//prepare for read in each line of given input file
+	string temp;
+	char command;
+	
+			//create a binary tree from the tree class to use in main
+	tree theTree;
+
+			//print header for output file
+	header();
+
+			//read in the very first line and extract the command
+	getline(input, temp, '\n');
+	command = temp[0];
+
+			//check commands and work accordingly
+	while (check(command)) {
+		item theItem;
+
+				//work according to the given command
+		switch (command) {
+		case 'I':
+					//insert an item for command 'I'
+
+					//read in all the given values into their respective variables
+			getline(input, theItem.ID, '\n');							//read the ID
+			getline(input, theItem.description, '\n');					//read the Description
+			getline(input, temp, '\n');									//read Quantity on Hand
+			theItem.onHand = stoi(temp);								//convert it into integer value
+			getline(input, temp, '\n');									//read Quantity on Order
+			theItem.onOrder = stoi(temp);								//convert it into integer value
+
+					//insert the item in the tree
+			if (theTree.add(theItem)) print ('A', true, theItem);		//notify success	
+			else print('A', false, theItem);							//notify duplicate
+
+					//print 60 '-'s
+			print('-');
+
+			break;
+
+		case 'D':
+					//delete an item for command 'D'
+
+					//read the given values
+			getline(input, theItem.ID, '\n');							//read the ID
+			getline(input, theItem.description, '\n');					//read the Description
+
+					//delete the described item
+			if (theTree.del(theItem)) print('D', true, theItem);		//notify success
+			else print('D', false, theItem);							//notify error
+
+					//print 60 '-'s
+			print('-');
+
+			break;
+
+		case 'P':
+					//print for command 'P'
+
+					//read in the given command to print everything(E) or one node(N)
+			getline(input, temp, '\n');
+
+					//print everything for 'E'
+			if (temp[0] == 'E') {
+						
+						//print the header for item list
+				print('H');
+						//print the entire tree
+				print('E', &theTree);
+			}
+					//print one particular node for 'N'
+			else if (temp[0] == 'N') {
+
+						//read the given value which entry to print
+				getline(input, theItem.ID, '\n');
+
+						//check if the entry is in the list
+				item *found = theTree.search(theItem);
+
+						//print the result if the item is in tree
+				if (found != NULL) {
+							//print header for the item
+					print('H');
+							//print the item
+					print('N', true, *found);
+				}
+				else {
+							//print error message if the item is not in tree
+					print('N', false, theItem);
+							//print 60 '-'s
+					print('-');
+				}
+			}
+
+			break;
+
+		case 'S':
+					//count items sold for command 'S'
+					
+					//read in the given values
+			getline(input, theItem.ID, '\n');				//read item ID to update
+			getline(input, temp, '\n');						//read in the quantity to update
+			theItem.quantity = stoi(temp);					//convert it into integer
+
+					//print success message if item is valid and updated
+			if (theTree.sell(theItem)) print('S', true, theItem);
+
+					//print error message if updating did not happen
+			else print('S', false, theItem);
+
+					//print 60 '-'s
+			print('-');
+
+			break;
+
+		case 'O':
+					//count items ordered for command 'O'
+
+					//read in the given values
+			getline(input, theItem.ID, '\n');				//read item ID to update
+			getline(input, temp, '\n');						//read in the quantity to update
+			theItem.quantity = stoi(temp);					//convert it into integer
+
+					//print success message if item is valid and updated
+			if (theTree.order(theItem)) print('O', true, theItem);
+
+					//print error message if updating did not happen
+			else print('O', false, theItem);
+
+					//print 60 '-'s
+			print('-');
+
+			break;
+
+		case 'R':
+					//count items received for command 'R'
+
+					//read in the given values
+			getline(input, theItem.ID, '\n');				//read item ID to update
+			getline(input, temp, '\n');						//read in the quantity to update
+			theItem.quantity = stoi(temp);					//convert it into integer
+
+					//print success message if item is valid and updated
+			if (theTree.receive(theItem)) print('R', true, theItem);
+
+					//print error message if updating did not happen
+			else print('R', false, theItem);
+
+					//print 60 '-'s
+			print('-');
+
+			break;
+
+		}
+
+				//read the next command to perform
+		getline(input, temp, '\n');
+		command = temp[0];
+	}
+			//print footer at the end of the output file
+	footer();
+
+	return 0; 
+
+}
